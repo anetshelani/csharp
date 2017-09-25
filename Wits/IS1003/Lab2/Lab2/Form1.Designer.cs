5018be66632bef4f57f6e11b929571352010c2a6
anetshelani <anetshelani.maselesele@gmail.com>
2017-09-25 20:58:34 +0200

    updated code

--- Wits/IS1003/Lab2/Lab2/Form1.Designer.cs
+++ Wits/IS1003/Lab2/Lab2/Form1.Designer.cs
@@ -28,12 +28,254 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtContactNo = new System.Windows.Forms.TextBox();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.txtNoSessions = new System.Windows.Forms.TextBox();
+            this.lblDiscount = new System.Windows.Forms.Label();
+            this.lblVATAmt = new System.Windows.Forms.Label();
+            this.lblAmtOwing = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get Creative!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Surname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contact No";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Course";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "No Sessions";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Discount";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "VATAmt";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(25, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "AmtOwing";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(98, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(98, 80);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 10;
+            // 
+            // txtContactNo
+            // 
+            this.txtContactNo.Location = new System.Drawing.Point(98, 107);
+            this.txtContactNo.Name = "txtContactNo";
+            this.txtContactNo.Size = new System.Drawing.Size(100, 20);
+            this.txtContactNo.TabIndex = 11;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Location = new System.Drawing.Point(98, 139);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(100, 20);
+            this.txtCourse.TabIndex = 12;
+            // 
+            // txtNoSessions
+            // 
+            this.txtNoSessions.Location = new System.Drawing.Point(98, 170);
+            this.txtNoSessions.Name = "txtNoSessions";
+            this.txtNoSessions.Size = new System.Drawing.Size(100, 20);
+            this.txtNoSessions.TabIndex = 13;
+            // 
+            // lblDiscount
+            // 
+            this.lblDiscount.AutoSize = true;
+            this.lblDiscount.Location = new System.Drawing.Point(95, 199);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(28, 13);
+            this.lblDiscount.TabIndex = 14;
+            this.lblDiscount.Text = "0.00";
+            // 
+            // lblVATAmt
+            // 
+            this.lblVATAmt.AutoSize = true;
+            this.lblVATAmt.Location = new System.Drawing.Point(95, 235);
+            this.lblVATAmt.Name = "lblVATAmt";
+            this.lblVATAmt.Size = new System.Drawing.Size(28, 13);
+            this.lblVATAmt.TabIndex = 15;
+            this.lblVATAmt.Text = "0.00";
+            // 
+            // lblAmtOwing
+            // 
+            this.lblAmtOwing.AutoSize = true;
+            this.lblAmtOwing.Location = new System.Drawing.Point(95, 267);
+            this.lblAmtOwing.Name = "lblAmtOwing";
+            this.lblAmtOwing.Size = new System.Drawing.Size(28, 13);
+            this.lblAmtOwing.TabIndex = 16;
+            this.lblAmtOwing.Text = "0.00";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(81, 303);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 17;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(212, 304);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 18;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(347, 307);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 338);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAmtOwing);
+            this.Controls.Add(this.lblVATAmt);
+            this.Controls.Add(this.lblDiscount);
+            this.Controls.Add(this.txtNoSessions);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtContactNo);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtContactNo;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.TextBox txtNoSessions;
+        private System.Windows.Forms.Label lblDiscount;
+        private System.Windows.Forms.Label lblVATAmt;
+        private System.Windows.Forms.Label lblAmtOwing;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
     }
 }
 
--- Wits/IS1003/Lab2/Lab2/Form1.cs
+++ Wits/IS1003/Lab2/Lab2/Form1.cs
@@ -16,5 +16,88 @@ namespace Lab2
         {
             InitializeComponent();
         }
+        const double TRIALCOST = 100;
+        const double VATRATE = 0.1;
+        bool blnValidInput=true;
+        string InvalidInput = "You have entered an invalid value for:";
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double dblNoSessions = 0;
+            double dblVATAmt=0;
+            double dblAmtOwing=0;
+
+            bool NoSessions = Double.TryParse(txtNoSessions.Text, out dblNoSessions);
+            if (NoSessions == false)
+            { MessageBox.Show("Invalid Input", "Error"); }
+
+            ValidateInput();
+
+            
+
+            if (blnValidInput == true)
+            {
+                dblAmtOwing=CalcAmtOwing(dblNoSessions);
+                lblAmtOwing.Text = dblAmtOwing.ToString("C2");
+
+                dblVATAmt = CalcVATAmt(dblAmtOwing);
+                lblVATAmt.Text = dblVATAmt.ToString("C2");
+            }
+
+        }   
+        private void ValidateInput()
+        {
+            double dblNoSessions = 0;
+
+            bool NoSessions = Double.TryParse(txtNoSessions.Text, out dblNoSessions);
+            if (NoSessions == false)
+            { MessageBox.Show("Invalid Input", "Error"); }
+
+            if (txtName.Text == "")
+            {
+                InvalidInput = InvalidInput + "Name,";
+                blnValidInput = false;
+            }
+
+            if (txtSurname.Text=="")
+            {
+                InvalidInput=InvalidInput + "Surname,";
+                blnValidInput=false;
+            }
+
+            if (txtContactNo.Text == "")
+            { 
+                InvalidInput = InvalidInput + "Contact No,";
+            blnValidInput = false;
+            }
+
+            if (dblNoSessions<1)
+            {InvalidInput=InvalidInput + "No of sessions";
+           blnValidInput=false;}
+
+            if (txtCourse.Text != "TRIAL")
+            { InvalidInput = InvalidInput + "Course"; }
+
+            if (blnValidInput == false)
+            { MessageBox.Show(InvalidInput); }
+        }
+
+        private double CalcAmtOwing(double dblNoSessions)
+        {
+            double dblAmtOwing = 0;
+            dblNoSessions = Convert.ToDouble(txtNoSessions.Text);
+            dblAmtOwing = dblNoSessions * TRIALCOST;
+            return dblAmtOwing;
+        }
+
+        private double CalcVATAmt(double dblAmtOwing)
+        {
+            double dblVATAmt = 0;
+
+            dblVATAmt = dblAmtOwing * VATRATE;
+            return dblVATAmt;
+                
+        }
+        
     }
 }
